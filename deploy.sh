27e7a65e3bee9e06c9a15bfedf78a5bd098fe43b
Toy Savi <savi.toy@amkcambodia.com>
2025-09-15 11:11:53 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -2,24 +2,29 @@
 # A script to deploy the application
 echo "Starting deployment..."
 
-kubectl create namespace blue || echo "Namespace 'blue' already exists."
+
 
 # Pull the latest changes from the repository
 git fetch origin && git checkout main
 
 echo "Pulled latest changes from repository."
 
+
+kubectl create namespace blue || echo "Namespace 'blue' already exists."
+
 # Deploy ingress 'nginx'
 kubectl apply -f ingress/blue-ingress.yaml
-echo "Deployed ingress 'nginx'."
+
 
 # Deploy configmap
 kubectl apply -f configmap/blue-configmap.yaml
-echo "Deployed configmap."
+
 
 # Deploy the blue version of the application
 kubectl apply -f deployment/blue-deployment.yaml
-echo "Deployed blue version of the application."
+
+# Expose the blue version of the application via a LoadBalancer service
+kubectl apply -f deployment/blue-service.yaml
 
 echo "Deployment completed successfully."
 
